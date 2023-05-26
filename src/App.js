1ea74d8cb3d87994e215e99c914ef0de3402578c
Yoann PERIQUOI <yoann_63115@hotmail.fr>
2023-05-26 15:37:28 +0200

    Base structure and add of ressources

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello World !</p>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
